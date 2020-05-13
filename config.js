21965945baa431f7a5a3381288e9cd318c3c577d
Katy Solovewicz <katy@solovewicz.com>
2020-05-12 21:47:59 -0700

    make minor style adjustments

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Katy Solovewicz',
   heading: 'Software Developer',
   // social
